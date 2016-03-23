62b7e273bb3357a78fa73e951c55f640dd7aa2fc
hardac <hardac@163.com>
2016-03-23 08:06:22 +0800

    实现了鱼的动画

--- src/app/views/FishSprite.lua
+++ src/app/views/FishSprite.lua
@@ -1,5 +1,5 @@
 
-local FishSprite = class("FishSprite", function(imageFilename, plistFileName, startImageName, stringFormat, startFrameIndex,endFameIndex)
+local FishSprite = class("FishSprite", function(imageFilename, plistFileName, startImageName, stringFormat, startFrameIndex,endFameIndex, positionX,positionY)
     local cache = cc.SpriteFrameCache:getInstance()
     local filename = "fish3"
    
@@ -16,12 +16,34 @@ local FishSprite = class("FishSprite", function(imageFilename, plistFileName, st
 
     
     local animMixed = cc.Animation:createWithSpriteFrames(moreFrames, 0.3)
-    sprite:align(display.CENTER,display.cx,display.cy)
+    sprite:runAction(cc.RepeatForever:create( cc.Animate:create(animMixed) ) )
+    sprite:align(display.CENTER,positionX,positionY)
     return sprite;
 end)
 
-function FishSprite:setPosition(positionX, positionY)
-    self.align(display.CENTER,positionX,positionY)
+function FishSprite:ctor(imageFilename, fishModel)
+    self.model = fishModel
+end
+
+function FishSprite:getModel()
+    return self.model
+end
+
+function FishSprite:start(destination)
+    self.model:setDestination(destination)
+    self:updatePosition()
+    return self
+end
+
+function FishSprite:step(dt)
+    self.model:step(dt)
+    self:updatePosition()
+    return self
+end
+
+function FishSprite:updatePosition()
+    self:move(self.model:getPosition())
+        :rotate(self.model:getRotation())
 end
 
 return FishSprite
--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -1,30 +1,17 @@
 
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
-local DeadBugSprite = import(".FishSprite")
+local FishSprite = import(".FishSprite")
 MainScene.RESOURCE_FILENAME = "battleScene.csb"
 
 function MainScene:onCreate()
     printf("resource node = %s", tostring(self:getResourceNode()))
-    local cache = cc.SpriteFrameCache:getInstance()
-    local filename = "fish3"
-   
-    cache:addSpriteFrames("fish3.plist", "fish3.png")
 
-   local sprite = display.newSprite("#bk_01.png")
- 
-
-    local moreFrames = {}
-    for i = 1,14 do
-        local frame = cache:getSpriteFrame(string.format("bk_%02d.png",i))
-        moreFrames[i] = frame
-    end
-
-    
-    local animMixed = cc.Animation:createWithSpriteFrames(moreFrames, 0.3)
-     sprite:align(display.CENTER,display.cx,display.cy)
+     local sprite = FishSprite:create("fish3.png", "fish3.plist", "#bk_01.png", "bk_%02d.png", 1, 14, display.cx, display.cy)
+     
+    --sprite:align(display.CENTER,positionX,positionY)
     sprite:addTo(self)
   
-    sprite:runAction(cc.RepeatForever:create( cc.Animate:create(animMixed) ) )
+    --sprite:runAction(cc.RepeatForever:create( cc.Animate:create(animMixed) ) )
    -- self:addChild(sprite)
 
    
